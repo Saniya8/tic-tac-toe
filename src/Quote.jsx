9c050effb7c20d486682ee2f33c838425c6d3426
Saniya8 <saniyazad75@gmail.com>
2023-11-24 01:30:37 +0530

    latest

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import './Quote.css'
-import Image from './assets/image_2.png'
+import Image from './assets/image.png'
+import Image2 from './assets/image_2.png'
 
 function Quote() {
 
     const [quote, setQuote] = useState('Loading..');
+    const [count, setCount] = useState('');
 
     const fetchquotes = async () => {
         try {
             const fetchdata = await fetch('https://api.adviceslip.com/advice')
             const data = await fetchdata.json();
             console.log(data.slip.advice)
+            setCount(data.slip.id)
             setQuote(data.slip.advice)
         }
         catch (err) {
@@ -28,10 +31,15 @@ function Quote() {
     }, [])
 
     return (
-        <div className='quote-container'>
-            <h1 className='quote-title'>Quote #1</h1>
-            <p className='quote-p'>"{quote}"</p>
-            <img src={Image} alt="image" />
+        <div className='quote'>
+            <div className='quote-container'>
+                <h1 className='quote-title'>Quote #<span>{count}</span></h1>
+                <p className='quote-p'>"{quote}"</p>
+                <div className='quote-img'>
+                    <img src={Image} alt="image" />
+                    {/* <img src={Image2} alt="image_2" /> */}
+                </div>
+            </div>
         </div>
     )
 }
